4e94bf5d70963d8d499cdeadfaeec274f97c463b
CosmicLM <edsldev25@gmail.com>
2026-03-02 23:05:37 -0700

    add: add new variable in config.py

--- config.py
+++ config.py
@@ -5,12 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Identity & Neural Link
-api_key = os.getenv("HYPR_API_KEY")
-if not api_key:
+API_KEY = os.getenv("HYPR_API_KEY")
+if not API_KEY:
     print("CRITICAL: HYPR_API_KEY not found.")
     sys.exit(1)
     
-voice_model = os.getenv("DEFAULT_VOICE_MODEL")
+VOICE_MODEL = os.getenv("DEFAULT_VOICE_MODEL")
+
+AI_MODEL = "gemini-3-flash-preview"
 
 # Import system-wide constants to ensure consistent behavior across all Hypr modules.
 NULL_COMMAND = "null"
